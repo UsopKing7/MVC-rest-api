8051633b2c5900b92c70ca0ad3d9e3c44c4aa9ed
UsopKing7 <programadorusop@gmail.com>
2025-03-08 17:15:58 -0400

    agregando error 404 al server

--- server.js
+++ server.js
@@ -2,13 +2,16 @@
 
 import express from 'express'
 import { routerControllers } from './src/controllers/routes.js'
-import { routerDb } from './src/modules/db-controllers.routes.js'
 
 const app = express()
 app.use(express.json())
+app.disable('x-powered-by')
 
-routerControllers.forEach((router) => app.use('/moduls', router))
-app.use('/db', routerDb)
+routerControllers.forEach((router) => app.use('/db', router))
+
+app.use((req, res) => {
+  return res.status(404).json({ message: "error 404 not font"})
+})
 
 const port = process.env.PORT ?? 3333
 app.listen(port, () => {
