7d10b714d9ff9d8fe88656c43f9e8d75a813c230
UsopKing7 <programadorusop@gmail.com>
2025-03-08 00:38:50 -0400

    agragando el try catch

--- src/modules/db-controllers.routes.js
+++ src/modules/db-controllers.routes.js
@@ -6,7 +6,11 @@ import { pool } from './db-pool.routes.js'
 export const routerDb = Router()
 
 routerDb.post('/', async(req, res) => {
-  const { name, brand, category } = req.body
-  const [data] = await pool.query('INSERT INTO cellPhones (name, brand, category) VALUES (?, ?, ?)', [name, brand, category])
-  return res.status(201).json({data})
+  try {
+    const { name, brand, category } = req.body
+    const [data] = await pool.query('INSERT INTO cellPhones (name, brand, category) VALUES (?, ?, ?)', [name, brand, category])
+    return res.status(201).json({data})
+  } catch (error) {
+    return res.status(404).json({ message: error.errors})
+  }
 })
