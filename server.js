d6c3190d08759f3f2bf9eae0d2d88ea6b7c200f7
UsopKing7 <programadorusop@gmail.com>
2025-03-08 00:18:21 -0400

    mejoras en el server y json

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
 
 import express from 'express'
-import { routerGet } from './src/controllers/get-controllers.routes.js'
-import { routerPost } from './src/controllers/post-controllers.routes.js'
-import { routerDb } from './src/modules/db-controllers.routes.js';
+import { routerControllers } from './src/controllers/routes.js'
+import { routerDb } from './src/modules/db-controllers.routes.js'
 
 const app = express()
 app.use(express.json())
 
+routerControllers.forEach((router) => app.use('/moduls', router))
 app.use('/db', routerDb)
-app.use('/moduls', routerPost)
-app.use('/moduls', routerGet)
 
 const port = process.env.PORT ?? 3333
 app.listen(port, () => {
-  console.table('server => http://localhost:3333')
+  console.log('server => http://localhost:3333')
 })
