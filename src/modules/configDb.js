810bc53000a0b74eb831a57636e959a7ee4713a4
UsopKing7 <programadorusop@gmail.com>
2025-03-09 22:11:45 -0400

    variable de entorno

--- src/modules/configDb.js
+++ src/modules/configDb.js
@@ -8,3 +8,4 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || 'nicolas'
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_DATABASE = process.env.DB_DATABASE || 'cellPhonesCompany'
 export const DB_PORT = process.env.DB_PORT || 3306
+export const PORT = process.env.PORT ||3333
